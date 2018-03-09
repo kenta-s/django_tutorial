f7e5e6e9067e5074cbd29eed71de33073ea827ab
kenta-s <knt01222@gmail.com>
2018-03-10 08:20:10 +0900

    Implement detail form

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
 from .models import Question
 
 def index(request):
@@ -17,4 +19,12 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("you are voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, choice.DoesNotExist):
+        return render(request, 'polls/detail.html', { 'question': question, 'error_message': "You didn't select a choice.",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
